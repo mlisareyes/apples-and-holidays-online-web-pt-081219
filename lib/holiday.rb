d589d15efb0222a7d62c0e31f77bd9568680d8b9
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-02 20:01:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{holiday.capitalize}: #{supplies.flatten}"
+      puts "#{holiday.capitalize}: #{supplies.join(" ")}"
       binding.pry
     end
   end
