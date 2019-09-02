0da2a574567a4c39a4e2edf2b97ca965084bbb9c
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-02 20:58:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
+      if supply == "BBQ"
+        holiday
+      end
+      "richard"
     end
   end
 end
